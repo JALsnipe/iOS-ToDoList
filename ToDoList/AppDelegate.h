d973d2e2590d25be8d26364485b903cb02ab1ad6
Josh Lieberman <josh.lieberman92@gmail.com>
2014-03-29 13:26:52 -0400

    Initial Commit

---
+++ ToDoList/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Josh Lieberman on 3/29/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
